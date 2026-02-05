785d741f817d74c5120bd8dd874f9089fbd56d6e
RishitAgrawal234 <76435455+RishitAgrawal234@users.noreply.github.com>
2026-02-06 00:31:23 +0530

    Add script.js for home HTML and random category logic

---
+++ script.js
@@ -0,0 +1,23 @@
+var allCategoriesUrl = "https://davids-restaurant.herokuapp.com/categories.json";
+
+function buildAndShowHomeHTML (categories) {
+
+  $ajaxUtils.sendGetRequest(
+    homeHtml,
+    function (homeHtml) {
+
+      var chosenCategoryShortName = chooseRandomCategory(categories).short_name;
+
+      var homeHtmlToInsertIntoMain = insertProperty(homeHtml, 
+                                                    "randomCategoryShortName", 
+                                                    "'" + chosenCategoryShortName + "'");
+
+      insertHtml("#main-content", homeHtmlToInsertIntoMain);
+    },
+    false);
+}
+
+function chooseRandomCategory (categories) {
+  var randomArrayIndex = Math.floor(Math.random() * categories.length);
+  return categories[randomArrayIndex];
+}
